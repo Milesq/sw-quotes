7e66a113d29fa2a9d38c34754955475686f0b355
Milesq <wmilosz88@gmail.com>
2020-12-27 00:07:08 +0100

    feat: scan movie files

---
+++ src/read_files.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+func ReadFiles(dir string) (ret []string) {
+	files, err := ioutil.ReadDir("bin")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		parts := strings.Split(file.Name(), ".")
+		endCursor := len(parts) - 1
+		name, ext := parts[:endCursor], parts[endCursor]
+
+		if ext == "srt" {
+			ret = append(ret, strings.Join(name, "."))
+		}
+	}
+
+	return
+}
