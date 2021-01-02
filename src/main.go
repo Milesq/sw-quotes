7f4d6c1b7b7c9e186e1e02073e5c764cbafd7770
Milesq <wmilosz88@gmail.com>
2021-01-02 14:43:25 +0100

    feat: recognize named scene

--- src/main.go
+++ src/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"os"
+
 	"github.com/milesq/sw-quotes/src/config"
 	"github.com/milesq/sw-quotes/src/movie"
 )
@@ -11,11 +15,13 @@ func main() {
 	globalCfg, err := config.ParseFromFile("./movies.config.yml")
 
 	if err != nil {
-		panic(err)
+		panic("cannot parse from file")
 	}
 
 	// fmt.Println(config.ParseFromFile("./movies.config.yml"))
-	movie.NewScenePtr(`"You turned her against me"-"I will do what I must"`, globalCfg)
-	movie.NewScenePtr(`#4"You turned her against me"(-2)[2]-"I will do what I must"(3)[3]`, globalCfg)
-	// movie.NewScenePtr(`vader:hallway`, globalCfg)
+	// movie.NewScenePtr(`"You turned her against me"-"I will do what I must"`, globalCfg)
+	// movie.NewScenePtr(`#4"You turned her against me"(-2)[2]-"I will do what I must"(3)[3]`, globalCfg)
+	s, err := movie.NewScenePtr(`luke:hallway`, globalCfg)
+	fmt.Println(s)
+	fmt.Println(errors.Is(err, os.ErrNotExist))
 }
--- src/movie/get_scene.go
+++ src/movie/get_scene.go
@@ -2,35 +2,51 @@ package movie
 
 import (
 	"fmt"
+	"os"
 	"regexp"
 	"strings"
+
+	"github.com/thoas/go-funk"
 
 	"github.com/milesq/sw-quotes/src/config"
 )
 
-// ScenePtr struct, contains data about search parameter
-type ScenePtr struct {
-	Name string
-	From string
-	To   string
-}
-
 // NewScenePtr creates Scene Ptr
-func NewScenePtr(s string, cfg config.Config) (b ScenePtr) {
+func NewScenePtr(s string, cfg config.Config) (config.ScenePtr, error) {
 	s = strings.ToLower(s)
 
-	// isNamedScene := regexp.MustCompile(`^[0-9a-z/\-\+\\_:]+$`).MatchString(s)
+	isNamedScene := regexp.MustCompile(`^[0-9a-z/\-\+\\_:]+$`).MatchString(s)
+	if isNamedScene {
+		getNamedScene(s, cfg)
+		scene, ok := getNamedScene(s, cfg)
+		if !ok {
+			return config.ScenePtr{}, os.ErrNotExist
+		}
+
+		return scene, nil
+	}
 
 	quoteWord := `"[\w\s]+"(\(\-?[0-9]+\))?(\[\d+\])?`
 	quoteRegexp := `^(#\d+)?` + quoteWord + `\-` + quoteWord + `$`
-	fmt.Println(quoteRegexp)
 	quote := regexp.MustCompile(quoteRegexp)
 
 	matchToQuote := quote.MatchString(s)
 
 	fmt.Printf("%v\n\n", matchToQuote)
 
-	return
+	return config.ScenePtr{}, nil
+}
+
+func getNamedScene(name string, cfg config.Config) (s config.ScenePtr, false bool) {
+	found := funk.Find(cfg, func(sc config.ScenePtr) bool {
+		return sc.Name == name
+	})
+
+	if found == nil {
+		return
+	}
+
+	return found.(config.ScenePtr), true
 }
 
 // func GetScene(scene ScenePtr) {}
