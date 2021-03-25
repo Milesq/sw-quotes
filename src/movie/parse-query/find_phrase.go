e2e89cf3edc95d84480b5413ccba9b52f22f4426
Milesq <wmilosz88@gmail.com>
2021-03-25 23:00:30 +0100

    feat: send error when find many scenes

--- src/movie/parse-query/find_phrase.go
+++ src/movie/parse-query/find_phrase.go
@@ -1,6 +1,9 @@
 package getscene
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/adrg/strutil"
 	"github.com/adrg/strutil/metrics"
 	"github.com/milesq/sw-quotes/src/srt"
@@ -21,6 +24,25 @@ func findPhrase(phrase string, movies []srt.MovieData) error {
 				foundedScene = append(foundedScene, scene)
 			}
 		}
+	}
+
+	if len(foundedScene) > 1 {
+		err := "we found multiple matching phrases. You have to precise which one you want to use\n"
+		for i, scene := range foundedScene {
+			err += fmt.Sprint(
+				"Scene ID: ",
+				i,
+				"\n\tMovie Name: ",
+				scene.FileName,
+				"\n\tMovie ID: ",
+				scene.MovieID,
+				"\n\tPhrase: ",
+				scene.Srts[0],
+				"\n\n",
+			)
+		}
+
+		return errors.New(err)
 	}
 
 	return nil
