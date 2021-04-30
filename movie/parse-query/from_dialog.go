218c7b6cff3a68517f1bd984921c16a7e0e280a6
Milesq <wmilosz88@gmail.com>
2021-04-30 16:50:22 +0200

    feat: add some special chars to query

--- movie/parse-query/from_dialog.go
+++ movie/parse-query/from_dialog.go
@@ -27,7 +27,7 @@ type Query struct {
 // FromDialogQuery .
 func FromDialogQuery(rawQuery string, movies []srt.MovieData) (config.ScenePtr, error) {
 	var s config.ScenePtr
-	quoteWord := `"([\w\s]+)"(\((\-?[0-9]+)\))?(\[(\d+)\])?`
+	quoteWord := `"([\w\s,;.!'&]+)"(\((\-?[0-9]+)\))?(\[(\d+)\])?`
 	quoteRegexp := `^(#(\d+))?` + quoteWord + `\-` + quoteWord + `$`
 	quote := regexp.MustCompile(quoteRegexp)
 
